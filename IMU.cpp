80e53a07628ac385c854f233df5863bfdfa99b94
Pegasus204 <ride.or.die.2215@gmail.com>
2017-09-04 23:06:55 +0900

    refactor and reindent IMU.cpp

--- IMU.cpp
+++ IMU.cpp
@@ -8,49 +8,63 @@
 
 void getEstimatedAttitude();
 
-void computeIMU () {
+void computeIMU ()
+{
   uint8_t axis;
   static int16_t gyroADCprevious[3] = {0,0,0};
   static int16_t gyroADCinter[3];
 
   uint16_t timeInterleave = 0;
-  #if ACC
-    ACC_getADC();
-    getEstimatedAttitude();
-  #endif
-  #if GYRO
-    Gyro_getADC();
-  #endif
+#if ACC
+  ACC_getADC();
+  getEstimatedAttitude();
+#endif
+#if GYRO
+  Gyro_getADC();
+#endif
   for (axis = 0; axis < 3; axis++)
     gyroADCinter[axis] =  imu.gyroADC[axis];
   timeInterleave=micros();
   annexCode();
   uint8_t t=0;
-  while((int16_t)(micros()-timeInterleave)<650) t=1; //empirical, interleaving delay between 2 consecutive reads
-  #ifdef LCD_TELEMETRY
-    if (!t) annex650_overrun_count++;
-  #endif
-  #if GYRO
-    Gyro_getADC();
-  #endif
-  for (axis = 0; axis < 3; axis++) {
-    gyroADCinter[axis] =  imu.gyroADC[axis]+gyroADCinter[axis];
-    // empirical, we take a weighted value of the current and the previous values
-    imu.gyroData[axis] = (gyroADCinter[axis]+gyroADCprevious[axis])/3;
-    gyroADCprevious[axis] = gyroADCinter[axis]>>1;
-    if (!ACC) imu.accADC[axis]=0;
-  }
-  #if defined(GYRO_SMOOTHING)
-    static int16_t gyroSmooth[3] = {0,0,0};
-    for (axis = 0; axis < 3; axis++) {
+  
+  //empirical, interleaving delay between 2 consecutive reads
+  while((int16_t)(micros()-timeInterleave)<650)
+    {
+      t=1;
+    }
+#ifdef LCD_TELEMETRY
+  if (!t)
+    {
+      annex650_overrun_count++;
+    }
+#endif
+#if GYRO
+  Gyro_getADC();
+#endif
+  for (axis = 0; axis < 3; axis++)
+    {
+      gyroADCinter[axis] =  imu.gyroADC[axis]+gyroADCinter[axis];
+      // empirical, we take a weighted value of the current and the previous values
+      imu.gyroData[axis] = (gyroADCinter[axis]+gyroADCprevious[axis])/3;
+      gyroADCprevious[axis] = gyroADCinter[axis]>>1;
+      if (!ACC)
+        {
+          imu.accADC[axis]=0;
+        }
+    }
+#if defined(GYRO_SMOOTHING)
+  static int16_t gyroSmooth[3] = {0,0,0};
+  for (axis = 0; axis < 3; axis++)
+    {
       imu.gyroData[axis] = (int16_t) ( ( (int32_t)((int32_t)gyroSmooth[axis] * (conf.Smoothing[axis]-1) )+imu.gyroData[axis]+1 ) / conf.Smoothing[axis]);
       gyroSmooth[axis] = imu.gyroData[axis];
     }
-  #elif defined(TRI)
-    static int16_t gyroYawSmooth = 0;
-    imu.gyroData[YAW] = (gyroYawSmooth*2+imu.gyroData[YAW])/3;
-    gyroYawSmooth = imu.gyroData[YAW];
-  #endif
+#elif defined(TRI)
+  static int16_t gyroYawSmooth = 0;
+  imu.gyroData[YAW] = (gyroYawSmooth*2+imu.gyroData[YAW])/3;
+  gyroYawSmooth = imu.gyroData[YAW];
+#endif
 }
 
 // **************************************************
@@ -77,13 +91,13 @@ void computeIMU () {
    unit = n power of 2 */
 // this one is also used for ALT HOLD calculation, should not be changed
 #ifndef ACC_LPF_FACTOR
-  #define ACC_LPF_FACTOR 4 // that means a LPF of 16
+#define ACC_LPF_FACTOR 4 // that means a LPF of 16
 #endif
 
 /* Set the Gyro Weight for Gyro/Acc complementary filter
    Increasing this value would reduce and delay Acc influence on the output of the filter*/
 #ifndef GYR_CMPF_FACTOR
-  #define GYR_CMPF_FACTOR 10 //  that means a CMP_FACTOR of 1024 (2^10)
+#define GYR_CMPF_FACTOR 10 //  that means a CMP_FACTOR of 1024 (2^10)
 #endif
 
 /* Set the Gyro Weight for Gyro/Magnetometer complementary filter
@@ -91,18 +105,21 @@ void computeIMU () {
 #define GYR_CMPFM_FACTOR 8 // that means a CMP_FACTOR of 256 (2^8)
 
 
-typedef struct  {
+typedef struct
+{
   int32_t X,Y,Z;
 } t_int32_t_vector_def;
 
-typedef struct  {
+typedef struct
+{
   uint16_t XL; int16_t X;
   uint16_t YL; int16_t Y;
   uint16_t ZL; int16_t Z;
 } t_int16_t_vector_def;
 
 // note: we use implicit first 16 MSB bits 32 -> 16 cast. ie V32.X>>16 = V16.X
-typedef union {
+typedef union
+{
   int32_t A32[3];
   t_int32_t_vector_def V32;
   int16_t A16[6];
@@ -110,30 +127,53 @@ typedef union {
 } t_int32_t_vector;
 
 //return angle , unit: 1/10 degree
-int16_t _atan2(int32_t y, int32_t x){
+int16_t _atan2(int32_t y, int32_t x)
+{
   float z = y;
   int16_t a;
   uint8_t c;
   c = abs(y) < abs(x);
-  if ( c ) {z = z / x;} else {z = x / z;}
-  a = 2046.43 * (z / (3.5714 +  z * z));
-  if ( c ){
-   if (x<0) {
-     if (y<0) a -= 1800;
-     else a += 1800;
-   }
-  } else {
-    a = 900 - a;
-    if (y<0) a -= 1800;
+  if ( c )
+    {
+      z = z / x;
+    }
+  else {
+    z = x / z;
   }
+  a = 2046.43 * (z / (3.5714 +  z * z));
+  if ( c )
+    {
+      if (x<0)
+        {
+          if (y<0)
+            {
+              a -= 1800;
+            }
+          else
+            {
+              a += 1800;
+            }
+        }
+    }
+  else
+    {
+      a = 900 - a;
+      if (y<0)
+        {
+          a -= 1800;
+        }
+    }
   return a;
 }
 
-float InvSqrt (float x){ 
-  union{  
+float InvSqrt (float x)
+{ 
+  union
+  {  
     int32_t i;  
     float   f; 
-  } conv; 
+  }
+  conv; 
   conv.f = x; 
   conv.i = 0x5f1ffff9 - (conv.i >> 1); 
   return conv.f * (1.68191409f - 0.703952253f * x * conv.f * conv.f);
@@ -142,34 +182,35 @@ float InvSqrt (float x){
 // signed16 * signed16
 // 22 cycles
 // http://mekonik.wordpress.com/2009/03/18/arduino-avr-gcc-multiplication/
-#define MultiS16X16to32(longRes, intIn1, intIn2) \
-asm volatile ( \
-"clr r26 \n\t" \
-"mul %A1, %A2 \n\t" \
-"movw %A0, r0 \n\t" \
-"muls %B1, %B2 \n\t" \
-"movw %C0, r0 \n\t" \
-"mulsu %B2, %A1 \n\t" \
-"sbc %D0, r26 \n\t" \
-"add %B0, r0 \n\t" \
-"adc %C0, r1 \n\t" \
-"adc %D0, r26 \n\t" \
-"mulsu %B1, %A2 \n\t" \
-"sbc %D0, r26 \n\t" \
-"add %B0, r0 \n\t" \
-"adc %C0, r1 \n\t" \
-"adc %D0, r26 \n\t" \
-"clr r1 \n\t" \
-: \
-"=&r" (longRes) \
-: \
-"a" (intIn1), \
-"a" (intIn2) \
-: \
-"r26" \
-)
+#define MultiS16X16to32(longRes, intIn1, intIn2)    \
+  asm volatile (                                    \
+                "clr r26 \n\t"                      \
+                "mul %A1, %A2 \n\t"                 \
+                "movw %A0, r0 \n\t"                 \
+                "muls %B1, %B2 \n\t"                \
+                "movw %C0, r0 \n\t"                 \
+                "mulsu %B2, %A1 \n\t"               \
+                "sbc %D0, r26 \n\t"                 \
+                "add %B0, r0 \n\t"                  \
+                "adc %C0, r1 \n\t"                  \
+                "adc %D0, r26 \n\t"                 \
+                "mulsu %B1, %A2 \n\t"               \
+                "sbc %D0, r26 \n\t"                 \
+                "add %B0, r0 \n\t"                  \
+                "adc %C0, r1 \n\t"                  \
+                "adc %D0, r26 \n\t"                 \
+                "clr r1 \n\t"                       \
+                :                                   \
+                  "=&r" (longRes)                   \
+                :                                   \
+                  "a" (intIn1),                     \
+                  "a" (intIn2)                      \
+                :                                   \
+                  "r26"                             \
+                 )
 
-int32_t  __attribute__ ((noinline)) mul(int16_t a, int16_t b) {
+int32_t  __attribute__ ((noinline)) mul(int16_t a, int16_t b)
+{
   int32_t r;
   MultiS16X16to32(r, a, b);
   //r = (int32_t)a*b; without asm requirement
@@ -177,7 +218,8 @@ int32_t  __attribute__ ((noinline)) mul(int16_t a, int16_t b) {
 }
 
 // Rotate Estimated vector(s) with small angle approximation, according to the gyro data
-void rotateV32( t_int32_t_vector *v,int16_t* delta) {
+void rotateV32( t_int32_t_vector *v,int16_t* delta)
+{
   int16_t X = v->V16.X;
   int16_t Y = v->V16.Y;
   int16_t Z = v->V16.Z;
@@ -189,13 +231,14 @@ void rotateV32( t_int32_t_vector *v,int16_t* delta) {
 
 static int16_t accZ=0;
 
-void getEstimatedAttitude(){
+void getEstimatedAttitude()
+{
   uint8_t axis;
   int32_t accMag = 0;
   float scale;
   int16_t deltaGyroAngle16[3];
   static t_int32_t_vector EstG = {0,0,(int32_t)ACC_1G<<16};
-  #if MAG
+#if MAG
     static t_int32_t_vector EstM;
   #else
     static t_int32_t_vector EstM = {0,(int32_t)1<<24,0};
@@ -213,7 +256,8 @@ void getEstimatedAttitude(){
   previousT = currentT;
 
   // Initialization
-  for (axis = 0; axis < 3; axis++) {
+  for (axis = 0; axis < 3; axis++)
+    {
     // valid as long as LPF_FACTOR is less than 15
     imu.accSmooth[axis]  = LPFAcc[axis]>>ACC_LPF_FACTOR;
     LPFAcc[axis]      += imu.accADC[axis] - imu.accSmooth[axis];
@@ -233,9 +277,12 @@ void getEstimatedAttitude(){
   // Apply complimentary filter (Gyro drift correction)
   // If accel magnitude >1.15G or <0.85G and ACC vector outside of the limit range => we neutralize the effect of accelerometers in the angle estimation.
   // To do that, we just skip filter, as EstV already rotated by Gyro
-  for (axis = 0; axis < 3; axis++) {
+  for (axis = 0; axis < 3; axis++)
+    {
     if ( (int16_t)(0.85*ACC_1G*ACC_1G/256) < (int16_t)(accMag>>8) && (int16_t)(accMag>>8) < (int16_t)(1.15*ACC_1G*ACC_1G/256) )
-      EstG.A32[axis] += (int32_t)(imu.accSmooth[axis] - EstG.A16[2*axis+1])<<(16-GYR_CMPF_FACTOR);
+      {
+        EstG.A32[axis] += (int32_t)(imu.accSmooth[axis] - EstG.A16[2*axis+1])<<(16-GYR_CMPF_FACTOR);
+      }
     accZ_tmp += mul(imu.accSmooth[axis] , EstG.A16[2*axis+1]);
     #if MAG
       EstM.A32[axis]  += (int32_t)(imu.magADC[axis] - EstM.A16[2*axis+1])<<(16-GYR_CMPFM_FACTOR);
@@ -243,9 +290,13 @@ void getEstimatedAttitude(){
   }
   
   if (EstG.V16.Z > ACCZ_25deg)
-    f.SMALL_ANGLES_25 = 1;
+    {
+      f.SMALL_ANGLES_25 = 1;
+    }
   else
-    f.SMALL_ANGLES_25 = 0;
+    {
+      f.SMALL_ANGLES_25 = 0;
+    }
 
   // Attitude of the estimated vector
   int32_t sqGX_sqGZ = mul(EstG.V16.X,EstG.V16.X) + mul(EstG.V16.Z,EstG.V16.Z);
@@ -270,7 +321,8 @@ void getEstimatedAttitude(){
   // projection of ACC vector to global Z, with 1G subtructed
   // Math: accZ = A * G / |G| - 1G
   accZ = accZ_tmp *  invG;
-  if (!f.ARMED) {
+  if (!f.ARMED)
+    {
     accZoffset -= accZoffset>>3;
     accZoffset += accZ;
   }  
@@ -284,16 +336,22 @@ void getEstimatedAttitude(){
 
 
 #define applyDeadband(value, deadband)  \
-  if(abs(value) < deadband) {           \
-    value = 0;                          \
-  } else if(value > 0){                 \
-    value -= deadband;                  \
-  } else if(value < 0){                 \
-    value += deadband;                  \
-  }
+  if(abs(value) < deadband)             \
+    {                                   \
+      value = 0;                        \
+    }                                   \
+  else if(value > 0)                    \
+    {                                   \
+      value -= deadband;                \
+    }                                   \
+  else if(value < 0)                    \
+    {                                   \
+      value += deadband;                \
+    }                                   \
 
 #if BARO
-uint8_t getEstimatedAltitude(){
+uint8_t getEstimatedAltitude()
+{
   int32_t  BaroAlt;
   static float baroGroundTemperatureScale,logBaroGroundPressureSum;
   static float vel = 0.0f;
@@ -302,10 +360,14 @@ uint8_t getEstimatedAltitude(){
   uint16_t dTime;
 
   dTime = currentT - previousT;
-  if (dTime < UPDATE_INTERVAL) return 0;
+  if (dTime < UPDATE_INTERVAL)
+    {
+      return 0;
+    }
   previousT = currentT;
 
-  if(calibratingB > 0) {
+  if(calibratingB > 0)
+    {
     logBaroGroundPressureSum = log(baroPressureSum);
     baroGroundTemperatureScale = ((int32_t)baroTemperature + 27315) * (2 * 29.271267f); // 2 *  is included here => no need for * 2  on BaroAlt in additional LPF
     calibratingB--;
